7850068aee6e43b42297d1581eb58aaa1c3ebc2b
cheunglaptinggg <cheunglaptinggg@gmail.com>
2025-04-13 20:47:14 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -59,8 +59,8 @@ document.addEventListener('DOMContentLoaded', () => {
          },
         {
              name: "Easter", url: "templates/2025Easter.png", // The main template
-             margins: { top: 250, bottom: 50, left: 35, right: 35 }, // Adjust if needed
-             padding: { top: 20, bottom: 20, left: 20, right: 20 }, // Adjust if needed
+             margins: { top: 100, bottom: 100, left: 35, right: 35 }, // Adjust if needed
+             padding: { top: 20, bottom: 20, left: 25, right: 25 }, // Adjust if needed
              background: { type: 'color', value: '#B7C8DC'}
          },
     ];
