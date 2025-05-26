5325af2230f8a3cffe070bdab368ebe9a8336d0f
cheunglaptinggg <cheunglaptinggg@gmail.com>
2025-05-26 14:07:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,6 @@ padding: { top: 50, bottom: 50, left: 50, right: 50 },
 background: { type: 'image', url: 'templates/BlueSky-bg.png' } // Example background image - MAKE SURE THIS FILE EXISTS!
 },
 {
-             name: "School",
              name: "School",url: null,
 margins: { top: 330, bottom: 330, left: 120 , right: 120 },
 padding: { top: 20, bottom: 20, left: 20, right: 20 },
